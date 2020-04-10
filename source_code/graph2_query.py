eb7ab53fadf81ba7feb30643710c7802689bff86
rakshitharj <59129773+rakshitharj@users.noreply.github.com>
2020-04-10 17:10:29 -0500

    Graph2 query
    Graph2 query to insert into graph_plot table and fetch the data based on pool_window and to/from LGA

---
+++ source_code/graph2_query.py
@@ -0,0 +1,37 @@
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+import pandas as pd
+
+try:
+    connection = mysql.connector.connect(user='root', password='rootroot',
+                              host='locations.c1vvuhtpuoui.us-west-1.rds.amazonaws.com',
+                              database='ride_sharing')
+    mySql_insert_query = """insert into graph_plot2 
+                            select t.pool_id, t.rideLabel, count(t.isRideShared), p.count_of_rides, p.pool_window
+                            from trip_details as t ,
+                            pool_details as p
+                            where t.pool_id=p.pool_id
+                            group by  t.pool_id
+                            order by t.pool_id  """
+
+    cursor = connection.cursor()
+    cursor.execute(mySql_insert_query)
+    connection.commit()
+    print(cursor.rowcount, "Record inserted successfully into ride_sharing table")
+
+    mysql_select="""select g.pool_window,g.rideLabel, avg ((( g.actual_rides-g.shared_rides) / g.actual_rides ) * 100)
+                        from graph_plot2 as g
+                        group by pool_window, rideLabel """
+
+    df = pd.read_sql(mysql_select,con=connection)
+    print(df)
+    cursor.close()
+
+except mysql.connector.Error as error:
+    print("Failed to insert record into Laptop table {}".format(error))
+
+finally:
+    if (connection.is_connected()):
+        connection.close()
+        print("MySQL connection is closed")
