715a7f15ab5d1de38faa3e8761b7a63fecfa9abc
rakshitharj <59129773+rakshitharj@users.noreply.github.com>
2020-04-21 23:53:31 -0500

    updated new query

--- source_code/graph3_query.py
+++ source_code/graph3_query.py
@@ -1,7 +1,8 @@
 import mysql.connector
-
+from mysql.connector import Error
+from mysql.connector import errorcode
 import pandas as pd
-from Project.source_code.output_graph import *
+from ride_sharing.source_code.output_graph import *
 
 try:
 
@@ -9,32 +10,33 @@ try:
     F10=0
     T5=0
     T10=0
-    connection = mysql.connector.connect(user='root', password='root',
+    connection = connection = mysql.connector.connect(user='root', password='root',
                               host='localhost',
                               database='ride_sharing')
 
 
-    mysql_select="""SELECT pool_window, rideLabel, AVG(time_taken)
-                    FROM pool_details 
-                    group by pool_window, rideLabel ;"""
+    mysql_select="""select pool_window, rideLabel, avg(time_taken)
+                    from pool_details
+                    group by pool_window, rideLabel"""
 
     df = pd.read_sql(mysql_select,con=connection)
     print(df)
 
     for i in df.index:
+        #print(df['AVG(time_taken)'][i])
         if (df['pool_window'][i] == 5):
             if (df['rideLabel'][i] == "From LaGuardia"):
-                F5=df['AVG(time_taken)'][i]
+                F5=df['avg(time_taken)'][i]
                 print("F5",F5)
             if (df['rideLabel'][i] == "To LaGuardia"):
-                T5 = df['AVG(time_taken)'][i]
+                T5 = df['avg(time_taken)'][i]
                 print("T5", T5)
         if (df['pool_window'][i] == 10):
             if (df['rideLabel'][i] == "From LaGuardia"):
-                F10=df['AVG(time_taken)'][i]
+                F10=df['avg(time_taken)'][i]
                 print("F10",F10)
             if (df['rideLabel'][i] == "To LaGuardia"):
-                T10 = df['AVG(time_taken)'][i]
+                T10 = df['avg(time_taken)'][i]
                 print("T10", T10)
     graph3(F5, F10, T5, T10)
 
@@ -45,4 +47,3 @@ finally:
     if (connection.is_connected()):
         connection.close()
         print("MySQL connection is closed")
-
